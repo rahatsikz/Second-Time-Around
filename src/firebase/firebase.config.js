2b23f4f1324c61e4ea87fa7f24df9ef027491e51
rahatsikz <rascal6271@gmail.com>
2023-02-27 19:40:26 +0600

    project setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKeWW7_3OHrl2ztBiDG3RRMyJJiNXxqKw",
+  authDomain: "rat-second-time-around.firebaseapp.com",
+  projectId: "rat-second-time-around",
+  storageBucket: "rat-second-time-around.appspot.com",
+  messagingSenderId: "855422665029",
+  appId: "1:855422665029:web:f147d7331c7f6c18b17e2c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

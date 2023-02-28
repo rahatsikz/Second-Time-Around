af329e249271cb1a0591bcb7112b521b9aafa8e8
rahatsikz <rascal6271@gmail.com>
2023-02-28 13:23:16 +0600

    testimonial added to homepage

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Categories from "../Categories/Categories";
+import UserReview from "../UserReview/UserReview";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Categories></Categories>
+      <UserReview></UserReview>
     </div>
   );
 };
---
+++ src/pages/Home/UserReview/UserReview.js
@@ -0,0 +1,43 @@
+import React from "react";
+import SingleReview from "./SingleReview";
+
+const UserReview = () => {
+  const reviews = [
+    {
+      id: 1,
+      name: "James Vince",
+      location: "New York",
+      photo:
+        "https://images.pexels.com/photos/4691234/pexels-photo-4691234.jpeg?auto=compress&cs=tinysrgb&w=640&h=750&dpr=1",
+      description:
+        " Recently i got to know about this webiste which selling 2nd hand mobile phones. Their site is good. Even I ordered a mobile phone from here. Hope to have a good service always",
+    },
+    {
+      id: 2,
+      name: "Kevin Spencer",
+      location: "Boston",
+      photo:
+        "https://images.pexels.com/photos/4827572/pexels-photo-4827572.jpeg?auto=compress&cs=tinysrgb&w=640&h=750&dpr=1",
+      description:
+        "I was looking to buy a mobile phone. I was searching here and there for a lucrative deal. Finally I visited this website. And I got to bought one phone. This site is exceptional and trustworthy",
+    },
+  ];
+  return (
+    <div className="container mx-auto my-12">
+      <h2 className="text-3xl font-semibold text-teal-500 text-center">
+        User Reviews
+      </h2>
+      <p className="text-lg text-center mt-2 max-w-sm mx-auto">
+        We care about our user and get connected to know their experience in
+        shopping with us
+      </p>
+      <div className="grid lg:grid-cols-2 gap-8 mt-8">
+        {reviews.map((rev) => (
+          <SingleReview key={rev.id} rev={rev}></SingleReview>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UserReview;

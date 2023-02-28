dc4290ec4fd0a23c812383d5753ed9dba56c3ee1
rahatsikz <rascal6271@gmail.com>
2023-02-28 13:25:41 +0600

    footer added

--- src/pages/Home/Categories/Category.js
+++ src/pages/Home/Categories/Category.js
@@ -15,7 +15,7 @@ const Category = ({ cat }) => {
         </figure>
         <div className="card-body">
           <h2 className="text-center text-2xl mb-4 font-medium"> {name} </h2>
-          <div className="card-actions justify-center">
+          <div className="card-actions justify-center mb-4">
             <button
               onClick={handleCategory}
               className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded border border-teal-600 px-5 text-sm font-medium tracking-wide text-teal-600 shadow-md shadow-teal-200 transition duration-300 hover:border-teal-600 hover:text-teal-600 hover:shadow-sm hover:shadow-teal-200 focus:border-teal-700 focus:text-teal-700 focus:shadow-sm focus:shadow-teal-200 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-teal-300 disabled:text-teal-300 disabled:shadow-none"

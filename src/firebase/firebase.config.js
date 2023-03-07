ec8b13cc168c574a7193ae3b6e61c8272d2333c8
rahatsikz <rascal6271@gmail.com>
2023-03-07 13:06:02 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKeWW7_3OHrl2ztBiDG3RRMyJJiNXxqKw",
-  authDomain: "rat-second-time-around.firebaseapp.com",
-  projectId: "rat-second-time-around",
-  storageBucket: "rat-second-time-around.appspot.com",
-  messagingSenderId: "855422665029",
-  appId: "1:855422665029:web:f147d7331c7f6c18b17e2c",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
